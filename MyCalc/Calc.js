531bc2c6a571cb16a92bca67a3984d787b87b5f4
iSwfe Jay <iSwfe@qq.com>
2017-09-02 17:52:21 +0800

    [Update] My Calculator Beta_2.1
    0.(补充上次忘记说的更新)所有图片资源压缩高达74.6%,提升加载速度,以后会更新至更高压缩率的webp资源.
    1.图标的上下间距调整,使界面更友好.
    2.增加直接键入功能键[+-*/]时自动在前面加上数字0的功能,使得计算更直观.
    3.正则表达式改为optsRegex变量来控制.
    4.加入键盘输入的提示,该功能不太推荐使用,因为或许不太稳定.
    5.调整大部分按键布局,应该更合理些吧2333

--- MyCalc/Calc.js
+++ MyCalc/Calc.js
@@ -6,6 +6,7 @@
  * 					由getNumArr()得出,该方法只由calc()调用.
  */
 var rstBoxID = "rstBox";
+var optsRegex = "[\\+\\-\\*\\/]";
 var isCalcing = true;	//表示正在运算的指示器
 var boxVal = "";
 var boxLen = boxVal.length;
@@ -26,6 +27,8 @@ function operate(opt)
 	boxVal = getBoxVal();
 	boxLen = boxVal.length;
 
+	if (0==boxLen)
+		addBoxChar("0");
 	switch(opt)
 	{
 	case 'Del':
@@ -38,11 +41,11 @@ function operate(opt)
 		break;
 	default:
 		//如果最后一个是"[+-*/]",则替换成现在的+-*/
-		if ( boxVal.charAt(boxLen-1).match("[\\+\\-\\*\\/]") )
+		if ( boxVal.charAt(boxLen-1).match(optsRegex) )
 			delBoxLast();
 		//寻找前面的运算符,有则立即计算
 		boxVal = document.getElementById(rstBoxID).value;	//更新下刚才可能删除LastOne的boxValue
-		if ( boxVal.substring(1).search("[\\+\\-\\*\\/]") >=0 )	//从[1]开始截到最后,再做[+-*/]的匹配,避免"-321"就满足的情况
+		if ( boxVal.substring(1).search(optsRegex) >=0 )	//从[1]开始截到最后,再做[+-*/]的匹配,避免"-321"就满足的情况
 			calc();
 
 		//当检测到isCalcing指示器关闭时,将其打开,以便继续运算
@@ -102,7 +105,7 @@ function getNumArr(orgBoxStr)	//originalBoxString:原始的Box框字符串
 		num1Char = "-";
 		newBoxStr = orgBoxStr.substring(1);
 	}//得到( (+)num1 )的newBoxStr
-	else if ( -1!=newBoxStr.lastIndexOf("-") && newBoxStr.search("[\\+\\-\\*\\/]")!=newBoxStr.lastIndexOf("-") )
+	else if ( -1!=newBoxStr.lastIndexOf("-") && newBoxStr.search(optsRegex)!=newBoxStr.lastIndexOf("-") )
 	{
 		num2Char = "-";
 		var temp1 = newBoxStr.substring(0, newBoxStr.lastIndexOf('-') );//截到last'-'处(不包括'-')
@@ -110,7 +113,7 @@ function getNumArr(orgBoxStr)	//originalBoxString:原始的Box框字符串
 		newBoxStr = temp1 + temp2;//两个temp拼接成(+)num1 [opt] (+)num2
 	}//得到( (+)num1和(+)num2 )的newBoxStr
 
-	var optIndex = newBoxStr.search("[\\+\\-\\*\\/]");
+	var optIndex = newBoxStr.search(optsRegex);
 	num1 = num1Char + newBoxStr.substring(0, optIndex);//截到[+-*/]处(不包括)
 	num2 = num2Char + newBoxStr.substring(optIndex+1);//从[+-*/]开始截到最后(不包括)
 //	alert(num1 +","+ num2);
@@ -157,6 +160,7 @@ function optClear()
 	if ( 0 == getBoxVal().length )
 	{
 		rstBoxID = "rstBox";
+		optsRegex = "[\\+\\-\\*\\/]";
 		boxVal = "";
 		boxLen = boxVal.length;
 		numArr = ["", 0, 0];
